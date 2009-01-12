21ef344922d5c83a2a6217ff0264d137c9c81df8
James Herdman <james.herdman@gmail.com>
2009-01-11 19:24:03 -0500

    Beginnings of my custom Emacs file

---
+++ emacs.d/init.el
@@ -0,0 +1,24 @@
+;; Some of this is lifted directly from the emacs-starter-kit. I would 
+;; have used it if I wasn't interested in learning something new.
+(setq user-mail-address "james.herdman@gmail.com")
+(setq dotfiles-dir (file-name-directory
+		    (or (buffer-file-name) load-file-name)))
+(add-to-list 'load-path dotfiles-dir)
+
+;; Stuff that should be required always
+
+;; Allows you to open a buffer of recently opened files
+(require 'recentf)
+
+;; Saves your place in a file, so when re-opening you start there
+(require 'saveplace)
+
+;; Translates ANSI escape sequences into faces
+(require 'ansi-color)
+
+;; Color themes for emacs are great!
+(add-to-list 'load-path "~/.emacs.d/color-themer")
+(require 'color-theme)
+(color-theme-intialize)
+(load-file "~/.emacs.d/color-themes/color-theme-sunburt.el")
+(color-theme-sunburst-tm)
