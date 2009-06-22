a9b63cb08bdd8c0ba8af9ee4e692e48e49ab372c
James Herdman <james.herdman@gmail.com>
2009-06-22 07:44:10 -0400

    Emacs starter kit

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,29 +1,78 @@
-;; Some of this is lifted directly from the emacs-starter-kit. I would 
-;; have used it if I wasn't interested in learning something new.
-(setq user-mail-address "james.herdman@gmail.com")
+;;; init.el --- Where all the magic begins
+;;
+;; Part of the Emacs Starter Kit
+;;
+;; This is the first thing to get loaded.
+;;
+;; "Emacs outshines all other editing software in approximately the
+;; same way that the noonday sun does the stars. It is not just bigger
+;; and brighter; it simply makes everything else vanish."
+;; -Neal Stephenson, "In the Beginning was the Command Line"
+
+;; Load path etc.
+
 (setq dotfiles-dir (file-name-directory
-		    (or (buffer-file-name) load-file-name)))
+                    (or (buffer-file-name) load-file-name)))
+
 (add-to-list 'load-path dotfiles-dir)
+(add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit"))
+(add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit/jabber"))
 
-;; Stuff that should be required always
+(setq autoload-file (concat dotfiles-dir "loaddefs.el"))
+(setq package-user-dir (concat dotfiles-dir "elpa"))
+(setq custom-file (concat dotfiles-dir "custom.el"))
 
-;; Allows you to open a buffer of recently opened files
+;; These should be loaded on startup rather than autoloaded on demand
+;; since they are likely to be used in every session
+
+(require 'cl)
+(require 'saveplace)
+(require 'ffap)
+(require 'uniquify)
+(require 'ansi-color)
 (require 'recentf)
 
-;; Saves your place in a file, so when re-opening you start there
-(require 'saveplace)
+;; backport some functionality to Emacs 22 if needed
+(require 'dominating-file)
 
-;; Translates ANSI escape sequences into faces
-(require 'ansi-color)
+;; this must be loaded before ELPA since it bundles its own
+;; out-of-date js stuff. TODO: fix it to use ELPA dependencies
+(load "elpa-to-submit/nxhtml/autostart")
 
-;; Color themes for emacs are great!
-(add-to-list 'load-path "~/.emacs.d/color-themer")
-(require 'color-theme)
-(color-theme-intialize)
-(load-file "~/.emacs.d/color-themes/color-theme-sunburt.el")
-(color-theme-sunburst-tm)
+;; Load up ELPA, the package manager
 
-(add-hook 'c-mode-common-hook
-  (lambda ()
-    (font-lock-add-keywords nil
-      '(("\\<\\(FIXME\\|TODO\\|BUG\\):" 1 font-lock-warning-face t)))))
+(require 'package)
+(package-initialize)
+(require 'starter-kit-elpa)
+
+;; Load up starter kit customizations
+
+(require 'starter-kit-defuns)
+(require 'starter-kit-bindings)
+(require 'starter-kit-misc)
+(require 'starter-kit-registers)
+(require 'starter-kit-eshell)
+(require 'starter-kit-lisp)
+(require 'starter-kit-perl)
+(require 'starter-kit-ruby)
+(require 'starter-kit-js)
+
+(regen-autoloads)
+(load custom-file 'noerror)
+
+;; Work around a bug on OS X where system-name is FQDN
+(if (eq system-type 'darwin)
+    (setq system-name (car (split-string system-name "\\."))))
+
+;; You can keep system- or user-specific customizations here
+(setq system-specific-config (concat dotfiles-dir system-name ".el")
+      user-specific-config (concat dotfiles-dir user-login-name ".el")
+      user-specific-dir (concat dotfiles-dir user-login-name))
+(add-to-list 'load-path user-specific-dir)
+
+(if (file-exists-p system-specific-config) (load system-specific-config))
+(if (file-exists-p user-specific-config) (load user-specific-config))
+(if (file-exists-p user-specific-dir)
+  (mapc #'load (directory-files user-specific-dir nil ".*el$")))
+
+;;; init.el ends here
