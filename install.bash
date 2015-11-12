e06d898924bc0bef073c9d4641f7ff3cebf7712d
James Herdman <james.herdman@gmail.com>
2015-11-12 15:41:59 -0500

    Force Symlink Creation

--- install.bash
+++ install.bash
@@ -11,15 +11,15 @@ for file in $DIR/*
 do
   if [[ -f $file ]] && [[ $file != *"${THIS}" ]]; then
     bn=`basename $file`
-    ln -s $file "${HOME}/.${bn}"
+    ln -sf $file "${HOME}/.${bn}"
   fi
 done
 
 CONFIG_DIR="${HOME}/.config"
 
 mkdir -p $CONFIG_DIR/nvim
-ln -s .vim $CONFIG_DIR/nvim
-ln -s .vimrc $CONFIG_DIR/nvim/init.vim
+ln -sf .vim $CONFIG_DIR/nvim
+ln -sf .vimrc $CONFIG_DIR/nvim/init.vim
 
 #curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
