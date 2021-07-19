de15afe2e25f3794c4d82848978437a5e6ed252a
James Herdman <james.herdman@gmail.com>
2021-07-18 20:30:21 -0400

    Use Trouble

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -35,6 +35,18 @@ return require('packer').startup(function()
     as = 'lspconfig'
   }
 
+  use {
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
+
   -- LSPINSTALL
   use { 'kabouzeid/nvim-lspinstall', requires = 'lspconfig' }
 
